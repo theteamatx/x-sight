542fafee0dc4e0af6850b3f985b66ac60bfb1c38
Meet-Shah <meetashah@google.com>
2024-01-10 10:42:14 +0000

    added instructions for supported python version, modified Dockerfile to have cloud-sdk as base image and working voltera-lotka demo

--- py/sight/demo/volterra_lotka.py
+++ py/sight/demo/volterra_lotka.py
@@ -71,16 +71,18 @@ def driver(sight: Sight) -> None:
   # logging.info('steps=%s', steps)
   data_structures.log_var('R', 0, sight)
   data_structures.log_var('F', 0, sight)
+  action = decision.decision_point('init', sight) #, default_params)
+  logging.info('action=%s', action)
+  print(len(steps))
   for idx in range(len(steps) - 1):
-    with SimulationTimeStep(
-        time_step_index=[idx],
-        time_step=steps[idx],
-        time_step_size=_LAST_TS.value / _NUM_ITERS.value,
-        time_step_units=sight_pb2.SimulationTimeStepStart.TSU_UNKNOWN,
-        sight=sight,
-    ):
-      action = decision.decision_point('init', sight) #, default_params)
-      logging.info('action=%s', action)
+    # with SimulationTimeStep(
+    #     time_step_index=[idx],
+    #     time_step=steps[idx],
+    #     time_step_size=_LAST_TS.value / _NUM_ITERS.value,
+    #     time_step_units=sight_pb2.SimulationTimeStepStart.TSU_UNKNOWN,
+    #     sight=sight,
+    # ):
+
       if idx == 0:
         r = action['R0']
         f = action['F0']
@@ -91,14 +93,17 @@ def driver(sight: Sight) -> None:
 
       dt = steps[idx + 1] - steps[idx]
       last_r = r
+    #   logging.info('%s:  dt=%s', idx, dt)
       r = r * (1 + alpha * dt - gamma * dt * f)
       f = f * (1 - beta * dt + delta * dt * last_r)
+    #   logging.info('%s:  r=%s, f=%s', idx, r, f)
 
-    logging.info('r=%s', r)
-    if math.isinf(r):
-      decision.decision_outcome('prey_pop', -1000, sight)
-    else:
-      decision.decision_outcome('prey_pop', r if r < 100 else 100-3*(r-100), sight)
+  logging.info('r=%s', r)
+  if math.isinf(r):
+    decision.decision_outcome('prey_pop', -1000, sight)
+  else:
+    decision.decision_outcome('prey_pop', r if r < 100 else 100-3*(r-100), sight)
+
       # with SimulationState({}, sight):
       #   data_structures.log_var('R', r, sight)
       #   data_structures.log_var('F', f, sight)
@@ -108,10 +113,9 @@ def main(argv: Sequence[str]) -> None:
   if len(argv) > 1:
     raise app.UsageError('Too many command-line arguments.')
 
-  with Sight(
-      sight_pb2.Params(
-          label='Volterra-Lotka',
-          bucket_name=f'{os.environ["PROJECT_ID"]}-sight',
+  with Sight(sight_pb2.Params(
+        label='Volterra-Lotka',
+        bucket_name=f'{os.environ["PROJECT_ID"]}-sight',
         text_output=True,
     )) as sight:
     # Simulation.run_decision_configuration(
@@ -131,8 +135,11 @@ def main(argv: Sequence[str]) -> None:
         driver_fn=driver,
         description = '''
 The Lotka-Volterra equations, also known as the Lotka-Volterra predator-prey model, are a pair of first-order nonlinear differential equations, frequently used to describe the dynamics of biological systems in which two species interact, one as a predator and the other as prey.
+
 The prey are assumed to have an unlimited food supply and to reproduce exponentially, unless subject to predation; this exponential growth is represented in the equation above by the term αx. The rate of predation on the prey is assumed to be proportional to the rate at which the predators and the prey meet; this is represented above by βxy. If either x or y is zero, then there can be no predation. With these two terms the prey equation above can be interpreted as follows: the rate of change of the prey's population is given by its own growth rate minus the rate at which it is preyed upon.
+
 The Lotka-Volterra predator-prey model makes a number of assumptions about the environment and biology of the predator and prey populations:[5]
+
 The prey population finds ample food at all times.
 The food supply of the predator population depends entirely on the size of the prey population.
 The rate of change of population is proportional to its size.
