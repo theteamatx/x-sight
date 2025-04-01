bf27038f091a9fa4e5caa550dffad1c4b7717e9b
Hrushikesh Makode <152846252+hrushikeshm-g@users.noreply.github.com>
2025-04-01 13:07:56 +0530

    update the config path logic (#81)
    * update the config path logic
    
    * absoute path added in multiple opt demo

--- py/sight/widgets/decision/utils.py
+++ py/sight/widgets/decision/utils.py
@@ -14,14 +14,17 @@
 """utility functions to be used in other functionalities."""
 
 import os
-import yaml
+
 from absl import flags
+import yaml
 
 FLAGS = flags.FLAGS
+
 
 def is_scalar(value):
   scalar_types = (int, float, str, bool, type(None), bytes)
   return isinstance(value, scalar_types)
+
 
 def load_yaml_config(file_path):
   print(f'loading file from {file_path}')
@@ -32,20 +35,27 @@ def load_yaml_config(file_path):
     print(f"Error: Config file not found at {file_path}")
     exit(1)
 
+
 def get_worker_version(question_label):
   #? this only works for single type worker attached to each question lable
   #? for multiple workers, need to update the logic
   # current_script_directory = os.path.dirname(os.path.abspath(__file__))
-  workers_config_path = os.getenv("WORKERS_CONFIG_PATH")#, "default/path/to/config.yaml")
+
+  # !!! NEED TO CORRECT THIS FUNCRION
+
+  return 'v1'
+
+  workers_config_path = os.getenv(
+      "WORKERS_CONFIG_PATH")  #, "default/path/to/config.yaml")
   print('workers_config_path from env: ', workers_config_path)
-  workers_config = load_yaml_config('/x-sight/'+workers_config_path)
-  optimizers_config_path = os.getenv("OPTIMIZERS_CONFIG_PATH")#, "default/path/to/config.yaml")
+  workers_config = load_yaml_config('/x-sight/' + workers_config_path)
+  optimizers_config_path = os.getenv(
+      "OPTIMIZERS_CONFIG_PATH")  #, "default/path/to/config.yaml")
   print('optimizers_config_path from env: ', optimizers_config_path)
-  optimizers_config = load_yaml_config('/x-sight/'+optimizers_config_path)
+  optimizers_config = load_yaml_config('/x-sight/' + optimizers_config_path)
 
   optimizer_config = optimizers_config[question_label]
   # as of now assuming only 1 worker_type for each question
   for worker, worker_count in optimizer_config['workers'].items():
     worker_details = workers_config[worker]
     return worker_details['version']
-
