9959f1a686ea44a879efeee4c20e7a227b67d65b
hrushikeshm-g <hrushikeshm@google.com>
2025-06-30 07:24:45 +0000

    fix: move the test_decision to integraton dir

--- py/sight/widgets/decision/decision.py
+++ py/sight/widgets/decision/decision.py
@@ -184,11 +184,11 @@ _SERVER_QUEUE_BATCH_SIZE = flags.DEFINE_integer(
     'batch size of the server queue for message queue',
 )
 
-_CACHE_MODE = flags.DEFINE_enum('cache_mode', 'none', [
+_CACHE_MODE = flags.DEFINE_enum('cache_mode', 'gcs', [
     'gcs',
     'redis',
     'gcs_with_redis',
-], 'Which Sight cache to use ? (default is none)')
+], 'Which Sight cache to use ? (default is gcs)')
 
 _CONFIG_PATH = flags.DEFINE_string(
     'config_path', get_config_dir_path(),
