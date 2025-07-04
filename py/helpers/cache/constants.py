729cb4c5310ee2143e14d3b1f1ec5f545855364c
hrushikeshm-g <hrushikeshm@google.com>
2025-07-04 07:46:25 +0000

    fix: redis constant updated

--- py/helpers/cache/constants.py
+++ py/helpers/cache/constants.py
@@ -1,19 +1,18 @@
 """Cache Constants Module."""
 
+import os
+
 
 class CacheType():
-  LOCAL = 'local'
   REDIS = 'redis'
   GCS = 'gcs'
-  NONE = 'none'
-  LOCAL_WITH_REDIS = 'local_with_redis'
   GCS_WITH_REDIS = 'gcs_with_redis'
 
 
 class RedisConstants:
   """Class to hold Redis-related configuration constants."""
 
-  REDIS_HOST = 'localhost'
-  REDIS_PORT = 1234  # custom redis port
-  REDIS_PASS = ''
-  REDIS_DB = ''
+  REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+  REDIS_PORT = os.environ.get('REDIS_PORT', '1234')
+  REDIS_PASS = os.environ.get('REDIS_PASS', '')
+  REDIS_DB = os.environ.get('REDIS_DB', '')
